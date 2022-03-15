bd228669578df05387052e98fb3e6067351dc96d
Pierrick Thébault <pierrick@regarde.org>
2022-03-15 18:01:07 +0100

    Build

---
+++ docs/src/examples/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import TreeTable from './treetable'
+import SelectTable from './selecttable'
+import SelectTreeTable from './selecttreetable'
+import FoldableTable from './foldabletable';
+
+export {
+  TreeTable,
+  SelectTable,
+  SelectTreeTable,
+  FoldableTable
+}
